37a66c9e88b233105ecf359a87887413a39be9af
Cydonia03 <10985212@uvu.edu>
2023-06-04 15:52:42 -0600

    Modified read to have checks to make sure a valid word is entered

--- code_base/IO_ops.py
+++ code_base/IO_ops.py
@@ -3,6 +3,20 @@ from memory_and_file_ops import Get_Value
 def Read(act_nums,MEM):
     #Read a word from the keyboard into a specific location in memory.
     word = input("Please enter a word: ")
+    while True:
+        if len(word) != 5:
+            print("Error, word length must be 5 charactes. Ex: '+1873'")
+            word = input("Please enter a word: ")
+        if word[0] not in ('+','-'):
+            print("Error, word must begin with '-' or '+'")
+        try:
+            int(word[2:5])
+            break
+        except ValueError:
+            print("Error, last 4 characters must be digits")
+            word = input("Please enter a word: ")
+        
+         
     MEM[act_nums] = word
     return MEM
 
