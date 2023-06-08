3e2ae9c62baed709c37b2b56e13c5606ea67a6b5
Cydonia03 <10985212@uvu.edu>
2023-06-07 21:09:20 -0600

    Fixed Load, Store, Read, and Write to handle improper inputs. Read is an infinite loop with the option to type 'q' to exit, the rest will exit upon encountering impropoer inputs/data.

--- code_base/IO_ops.py
+++ code_base/IO_ops.py
@@ -2,19 +2,27 @@ from memory_and_file_ops import Get_Value
 
 def Read(act_nums,MEM):
     #Read a word from the keyboard into a specific location in memory.
-    word = input("Please enter a word: ")
+    explanation = 'Type "q" to exit'
     while True:
+        word = input("Please enter a word: ")
+        if word in ['Q','q']:
+            print("Exiting program")
+            exit()
         if len(word) != 5:
             print("Error, word length must be 5 charactes. Ex: '+1873'")
-            word = input("Please enter a word: ")
+            print(explanation)
+            continue
         if word[0] not in ('+','-'):
             print("Error, word must begin with '-' or '+'")
+            print(explanation)
+            continue
         try:
-            int(word[2:5])
+            int(word[1:5])
             break
         except ValueError:
             print("Error, last 4 characters must be digits")
-            word = input("Please enter a word: ")
+            print(explanation)
+            continue
         
          
     MEM[act_nums] = word
@@ -23,5 +31,20 @@ def Read(act_nums,MEM):
 def Write(act_nums,MEM):
     #Write a word from a specific location in memory to screen.
     word = MEM[act_nums]
+    if isinstance(word,str) == False:
+        print("Error, invalid memory data. Exiting program")
+        exit()
+    if len(word) !=5:
+        print("Error, invalid data. Exiting program.")
+        exit()
+    if word[0] not in ('+','-'):
+        print('Error, word must begin with "+" or "-". Exiting program')
+        exit()
+    try:
+        int(word[1:5])
+    except ValueError:
+        print("Error, last 4 digits are not numbers. Exiting program")
+        exit()
+    
     print(word)
     return MEM
--- code_base/load_store_ops.py
+++ code_base/load_store_ops.py
@@ -3,10 +3,39 @@ from memory_and_file_ops import Get_Value
 def Load(act_nums,MEM,ACC):
     #Load a word from a specific location in memory into the accumulator.
     word = MEM[act_nums]
+    if isinstance(word,str) == False:
+        print("Error, invalid memory data. Exiting program")
+        exit()
+    if len(word) !=5:
+        print("Error, invalid data. Exiting program.")
+        exit()
+    if word[0] not in ('+','-'):
+        print('Error, word must begin with "+" or "-". Exiting program')
+        exit()
+    try:
+        int(word[1:5])
+    except ValueError:
+        print("Error, last 4 digits are not numbers. Exiting program")
+        exit()
     ACC = word
     return MEM,ACC
 
 def Store(act_nums,MEM,ACC):
     #Store a word from the accumulator into a specific location in memory.
+    word = ACC
+    if isinstance(word,str) == False:
+        print("Error, invalid memory data. Exiting program")
+        exit()
+    if len(word) !=5:
+        print("Error, invalid data. Exiting program.")
+        exit()
+    if word[0] not in ('+','-'):
+        print('Error, word must begin with "+" or "-". Exiting program')
+        exit()
+    try:
+        int(word[1:5])
+    except ValueError:
+        print("Error, last 4 digits are not numbers. Exiting program")
+        exit()
     MEM[act_nums] = ACC
     return MEM, ACC
